6330620539f48a949a58f5f7da5efabf9ee2c5a2
Yeison <enmanuelquiterios@gmail.com>
2021-10-07 12:19:44 -0400

    commit tree 0b41fe38182ad4abb0d695815da50f97c528d0ca

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let num3 = 100;
+let num4 = 25;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 2) + 1;
+
+let mod = num3 / num4;
+
+let max = Math.max(10, 12, 20);
